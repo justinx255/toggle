285d21cdb5a93c9ded8178fe0a8e11c20078f177
justinx255 <sudhrashanraj45@gmail.com>
2021-08-27 09:28:06 +0530

    not working

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function change_text() {
     var name = document.getElementById("content").value;
     document.getElementById("demo").innerHTML = "Hello this page is done by Justin and you entered " + `<span class=tie>` + name + "</span>" + "&nbsp;&nbsp;" + "dude!!!!";
 }
-function clear_text() {
+function clearText() {
     document.getElementById("demo").innerHTML = "";
     document.getElementById("content").value = "";
 }
