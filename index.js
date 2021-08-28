60715189c303f572b82deed878e3c5048ef97387
justinx255 <sudhrashanraj45@gmail.com>
2021-08-28 18:34:42 +0530

    hai

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function change_text() {
     var name = document.getElementById("content").value;
-    document.getElementById("demo").innerHTML = "Hello this page is done by Justin and you entered " + `<span class=tie>` + name + "</span>" + "&nbsp;&nbsp;" + "dude!!!!";
+    document.getElementById("div").innerHTML = "Hello this page is done by Justin and you entered " + `<span class=tie>` + name + "</span>" + "&nbsp;&nbsp;" + "dude!!!!";
 }
 function clearText() {
-    document.getElementById("demo").innerHTML = "";
-    document.getElementById("content").value = "";
+    document.getElementById("div").innerHTML = "";
+    // document.getElementById("content").value = "";
 }
---
+++ jj.js
@@ -0,0 +1,18 @@
+const a = 10;
+function justin() {
+    // b=a+10;
+    console.log(v1);
+
+    if (true) {
+        var v1 = 1;
+
+        console.log(v1);
+    }
+
+}
+// function jjustin() {
+//     // var b=11;
+//     jus
+// }
+
+justin();
